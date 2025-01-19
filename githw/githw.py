5542ab52a46d87f6a5d03f9c4661998bcfc4f611
Daniel Kifer <dkifer@cse.psu.edu>
2025-01-19 13:26:44 -0500

    .

---
+++ githw/githw.py
@@ -0,0 +1,8 @@
+
+def main():
+    print("#######################################")
+    print("Git homework has not been completed :-(")
+    print("#######################################")
+
+if __name__ == "__main__":
+    main()
