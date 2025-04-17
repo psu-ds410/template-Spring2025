a176fbb985b355fafe52a84e52d580b2e8f281e1
Daniel Kifer <dkifer@cse.psu.edu>
2025-04-16 22:42:30 -0400

    .

--- spadoopframes/df/cities.scala
+++ spadoopframes/df/cities.scala
@@ -18,7 +18,7 @@ object Q3 {
     }
 
     def registerZipCounter(spark: SparkSession) = {
-        val zipCounter = udf({x: String => Option(x) match {case Some(y) => y.split(" ").size; case None => 0}})
+        val zipCounter = udf({x: String => Option(x) match {case Some(y) => y.split("\\s+").size; case None => 0}})
         spark.udf.register("zipCounter", zipCounter) // registers udf with the spark session
     }
 
